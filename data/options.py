5d106ed14d6a81f0592e491131b71a8c85e0cfdd
ll1zh <2901164005@qq.com>
2026-03-31 15:20:08 +0800

    Add files via upload

---
+++ data/options.py
@@ -0,0 +1,85 @@
+import argparse
+
+def option():
+    # Training settings
+    parser = argparse.ArgumentParser(description='CIDNet')
+    parser.add_argument('--batchSize', type=int, default=8, help='training batch size')
+    parser.add_argument('--cropSize', type=int, default=256, help='image crop size (patch size)')
+    parser.add_argument('--nEpochs', type=int, default=1000, help='number of epochs to CIDNet for end')
+    parser.add_argument('--start_epoch', type=int, default=0, help='number of epochs to start, >0 is retrained a pre-trained pth')
+    parser.add_argument('--snapshots', type=int, default=10, help='Snapshots for save checkpoints pth')
+    parser.add_argument('--lr', type=float, default=1e-4, help='Learning Rate')
+    parser.add_argument('--gpu_mode', type=bool, default=True)
+    parser.add_argument('--shuffle', type=bool, default=True)
+    parser.add_argument('--threads', type=int, default=16, help='number of threads for dataloader to use')
+
+    # choose a scheduler
+    parser.add_argument('--cos_restart_cyclic', type=bool, default=False)
+    parser.add_argument('--cos_restart', type=bool, default=True)
+
+    # warmup training
+    parser.add_argument('--warmup_epochs', type=int, default=3, help='warmup_epochs')
+    parser.add_argument('--start_warmup', type=bool, default=True, help='turn False to CIDNet without warmup')
+
+    # CIDNet datasets
+    parser.add_argument('--data_train_own'          , type=str, default='./datasets/SelfBuiltDataset/our485')
+    parser.add_argument('--data_train_lol_blur'     , type=str, default='./datasets/LOL_blur/CIDNet')
+    parser.add_argument('--data_train_lol_v1'       , type=str, default='./datasets/LOLv1/our485')
+    parser.add_argument('--data_train_lolv2_real'   , type=str, default='./datasets/LOLv2/Real_captured/Train')
+    parser.add_argument('--data_train_lolv2_syn'    , type=str, default='./datasets/LOLv2/Synthetic/Train')
+    parser.add_argument('--data_train_SID'          , type=str, default='./datasets/Sony_total_dark/CIDNet')
+    parser.add_argument('--data_train_SICE'         , type=str, default='./datasets/SICE/Dataset/CIDNet')
+    parser.add_argument('--data_train_fivek'        , type=str, default='./datasets/FiveK/CIDNet')
+
+    # validation input
+    parser.add_argument('--data_val_own'            , type=str, default='./datasets/SelfBuiltDataset/eval15/low')
+    parser.add_argument('--data_val_lol_blur'       , type=str, default='./datasets/LOL_blur/eval15/low_blur')
+    parser.add_argument('--data_val_lol_v1'         , type=str, default='./datasets/LOLv1/eval15/low')
+    parser.add_argument('--data_val_lolv2_real'     , type=str, default='./datasets/LOLv2/Real_captured/Test/Low')
+    parser.add_argument('--data_val_lolv2_syn'      , type=str, default='./datasets/LOLv2/Synthetic/Test/Low')
+    parser.add_argument('--data_val_SID'            , type=str, default='./datasets/Sony_total_dark/eval15/short')
+    parser.add_argument('--data_val_SICE_mix'       , type=str, default='./datasets/SICE/Dataset/eval15/test')
+    parser.add_argument('--data_val_SICE_grad'      , type=str, default='./datasets/SICE/Dataset/eval15/test')
+    parser.add_argument('--data_test_fivek'         , type=str, default='./datasets/FiveK/test/input')
+
+    # validation groundtruth
+    parser.add_argument('--data_valgt_own'          , type=str, default='./datasets/SelfBuiltDataset/eval15/high/')
+    parser.add_argument('--data_valgt_lol_blur'     , type=str, default='./datasets/LOL_blur/eval15/high_sharp_scaled/')
+    parser.add_argument('--data_valgt_lol_v1'       , type=str, default='./datasets/LOLv1/eval15/high/')
+    parser.add_argument('--data_valgt_lolv2_real'   , type=str, default='./datasets/LOLv2/Real_captured/Test/Normal/')
+    parser.add_argument('--data_valgt_lolv2_syn'    , type=str, default='./datasets/LOLv2/Synthetic/Test/Normal/')
+    parser.add_argument('--data_valgt_SID'          , type=str, default='./datasets/Sony_total_dark/eval15/long/')
+    parser.add_argument('--data_valgt_SICE_mix'     , type=str, default='./datasets/SICE/Dataset/eval15/target/')
+    parser.add_argument('--data_valgt_SICE_grad'    , type=str, default='./datasets/SICE/Dataset/eval15/target/')
+    parser.add_argument('--data_valgt_fivek'        , type=str, default='./datasets/FiveK/test/target/')
+
+    parser.add_argument('--val_folder', default='./results/', help='Location to save validation datasets')
+
+    # loss weights
+    parser.add_argument('--HVI_weight', type=float, default=1.0)
+    parser.add_argument('--L1_weight', type=float, default=1.0)
+    parser.add_argument('--D_weight',  type=float, default=0.5)
+    parser.add_argument('--E_weight',  type=float, default=50.0)
+    parser.add_argument('--P_weight',  type=float, default=1e-2)
+    
+    # use random gamma function (enhancement curve) to improve generalization
+    parser.add_argument('--gamma', type=bool, default=False)
+    parser.add_argument('--start_gamma', type=int, default=60)
+    parser.add_argument('--end_gamma', type=int, default=120)
+
+    # auto grad, turn off to speed up training
+    parser.add_argument('--grad_detect', type=bool, default=False, help='if gradient explosion occurs, turn-on it')
+    parser.add_argument('--grad_clip', type=bool, default=True, help='if gradient fluctuates too much, turn-on it')
+    
+    
+    # choose which dataset you want to CIDNet, please only set one "True"
+    parser.add_argument('--own', type=bool, default=False)
+    parser.add_argument('--lol_v1', type=bool, default=True)
+    parser.add_argument('--lolv2_real', type=bool, default=False)
+    parser.add_argument('--lolv2_syn', type=bool, default=False)
+    parser.add_argument('--lol_blur', type=bool, default=False)
+    parser.add_argument('--SID', type=bool, default=False)
+    parser.add_argument('--SICE_mix', type=bool, default=False)
+    parser.add_argument('--SICE_grad', type=bool, default=False)
+    parser.add_argument('--fivek', type=bool, default=False)
+    return parser
